164dd017f59558bc8cf47099df0facec8a04e6c9
ttchabs <tchabs334@gmail.com>
2024-04-18 21:58:40 +0200

    first commit
    simple upload of project, with main menu scene and actual game scene. inly script is the load scene script

---
+++ Assets/Scripts/Main Menu Start to actual game.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuStarttoactualgame : MonoBehaviour
+{
+    
+    public void PlayGame() 
+    {
+            SceneManager.LoadScene("Actual Game");
+    }
+    
+    
+
+}
+
